b0e34a3dcc0014f647c2325a46f42ed8a34f8f90
th1nksnow <buffallo654@yandex.ru>
2025-08-17 16:52:02 +0300

    Init server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
 func main() {
 
+	logger := log.New(os.Stdout, `serv `, log.LstdFlags|log.Lshortfile)
+
+	srv := server.NewServer(logger)
+
+	if err := srv.Server.ListenAndServe(); err != nil {
+		logger.Fatal(err)
+	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,37 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Server struct {
+	Logger *log.Logger
+	Server *http.Server
+}
+
+func NewServer(logger *log.Logger) *Server {
+
+	r := chi.NewRouter()
+
+	// Handlers
+	// ...
+	//
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      r,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	return &Server{
+		Logger: logger,
+		Server: srv,
+	}
+}
