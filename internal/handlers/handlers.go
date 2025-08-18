88cc62ad2c79e5198418490d456e8ef5b635b3e4
th1nksnow <114925852+th1nksnow@users.noreply.github.com>
2025-08-18 17:26:04 +0300

    Approve me >< (#1)
    * Init server
    
    * Add 1st handler
    
    * Add second handler
    
    * Add service logic & Switch Create to WriteFile
    
    * Fix text conversation
    
    * Processed the empty file error
    
    * Update internal/handlers/handlers.go
    
    Co-authored-by: Ivan Nizhnichenko <ivan_0805@mail.ru>
    
    ---------
    
    Co-authored-by: Ivan Nizhnichenko <ivan_0805@mail.ru>

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,54 @@
 package handlers
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func HandleRoot(res http.ResponseWriter, req *http.Request) {
+
+	path := "./index.html"
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		http.Error(res, "index.html is not found", http.StatusInternalServerError)
+		return
+	}
+	http.ServeFile(res, req, path)
+}
+
+func HandleUpload(res http.ResponseWriter, req *http.Request) {
+
+	file, _, err := req.FormFile("myFile")
+	if err != nil {
+		log.Print(err)
+		http.Error(res, "failed to recieve file", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		log.Print(err)
+		http.Error(res, "failed to read from file", http.StatusBadRequest)
+		return
+	}
+
+	if string(data) == "" {
+		http.Error(res, "recieved file is empty", http.StatusPaymentRequired)
+		return
+	}
+
+	convertedString := service.Convert(string(data))
+
+	err = os.WriteFile(time.Now().UTC().String(), []byte(convertedString), 0755)
+	if err != nil {
+		log.Print(err)
+http.Error(res, "internal server error", http.StatusInternalServerError)
+		return
+	}
+	res.Write([]byte(convertedString))
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,37 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+	"github.com/go-chi/chi/v5"
+)
+
+type Server struct {
+	Logger *log.Logger
+	Server *http.Server
+}
+
+func NewServer(logger *log.Logger) *Server {
+
+	r := chi.NewRouter()
+
+	r.Get("/", handlers.HandleRoot)
+	r.Post("/upload", handlers.HandleUpload)
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      r,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	return &Server{
+		Logger: logger,
+		Server: srv,
+	}
+}
